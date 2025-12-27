a072fa14ec7650d1630a982c2e421d6e068525c1
Lukas Bruechmann <lukas.bruechmann@stud.hs-flensburg.de>
2025-12-27 23:51:56 +0100

    Add nunjucks seperation of header, footer, layout for the pages

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import router from "./router.js";
 import { serveStaticFile } from "./middleware/serveStaticFile.js";
 
 const app = new Application();
+// Middleware
 app.use(serveStaticFile);
+// Routing
 app.use(router.routes());
 app.use(router.allowedMethods());
 
